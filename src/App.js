04de36a06a05ef4c82b84aa34eb5b5eb4f160918
paras9sid <sidharthjainp9@gmail.com>
2022-12-23 23:35:49 +0530

    div centered-dropdown box created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        
+        <h1>Should you use a dropdown?</h1>
+        <select className='dropdown-search-box'>
+          <option>Yes</option>
+          <option>Probably not!</option>
+        </select>
+        {/* </center> */}
+      </div>  
     </div>
   );
 }
